9012296e7b97f154de2a6e4982f98cce9b4faa4e
toelarry <to.larry92@gmail.com>
2019-03-16 16:28:19 -0700

    added styling and dom

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ function initMap() {
   var uluru = { lat: -25.344, lng: 131.036 };
   map = new google.maps.Map(document.getElementById('map'), { zoom: 4, center: uluru });
   marker = new google.maps.Marker({ position: uluru, map: map });
+  createButton('START', startTracking)
+  createButton('STOP', stopTracking)
+}
+
+function createButton(text, clickFunction) {
+  let button = document.createElement("button")
+  button.setAttribute('onclick', clickFunction)
+  button.innerHTML = text
+  document.querySelector('.buttons').append(button)
 }
 
 const trackMovement = () => {
